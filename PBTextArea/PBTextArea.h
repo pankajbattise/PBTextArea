fcca0768c0c1edf64815ca8d57269b19294f7a43
Pankaj Battise <pankaj.battise@extentia.com>
2021-10-28 18:54:50 +0530

    Initial Commit

---
+++ PBTextArea/PBTextArea.h
@@ -0,0 +1,19 @@
+//
+//  PBTextArea.h
+//  PBTextArea
+//
+//  Created by Pankaj Battise on 04/09/19.
+//  Copyright © 2019 Extentia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PBTextArea.
+FOUNDATION_EXPORT double PBTextAreaVersionNumber;
+
+//! Project version string for PBTextArea.
+FOUNDATION_EXPORT const unsigned char PBTextAreaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBTextArea/PublicHeader.h>
+
+
---
+++ PBTextArea/PBTextArea.swift
@@ -0,0 +1,22 @@
+//
+//  PBTextArea.swift
+//  PBTextArea
+//
+//  Created by Pankaj Battise on 04/09/19.
+//  Copyright © 2019 Extentia. All rights reserved.
+//
+
+public final class PBTextArea {
+    
+    let name = "PBTextArea"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
+
